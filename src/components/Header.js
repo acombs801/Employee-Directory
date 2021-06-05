857e6ccad5e1c2bf6d4ea3945971ac6977716a54
Adrian <74800016+acombs801@users.noreply.github.com>
2021-06-05 02:05:09 -0700

    created header function

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="headerTitle" style={{ marginLeft: "35px" }}>
+            <h2>Welcome to the Employee Directory</h2>
+        </div>
+    );
+}
+
+export default Header;
