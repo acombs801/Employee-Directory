c3f1a4a1f46afea466fecd3dcf3fd2d6dfa56f1e
Adrian <74800016+acombs801@users.noreply.github.com>
2021-06-05 02:06:55 -0700

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footerInfo" style={{ marginLeft: "40px" }}>
+            <h3>Employee-Directory created by <a href="https://github.com/acombs801">Adrian Combs</a></h3>
+        </div>
+    );
+}
+
+export default Footer;
