5e75ea133e6cb311b9d1ff64dd6ac310ebebc271
Adrian <74800016+acombs801@users.noreply.github.com>
2021-06-05 02:14:39 -0700

    added service worker started building table

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import EnhanceTable from "./components/EnhancedTable"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <EnhanceTable />
       <Footer />
     </div>
   );
---
+++ src/components/EnhancedTable.js
@@ -0,0 +1,18 @@
+export default function EnhancedTable() {
+    const [state, setState] = React.useState({
+        columns: [
+            { title: 'Name', field: 'name' },
+            { title: 'Surname', field: 'surname' },
+            { title: 'Employee ID', field: 'employeeID', type: 'numeric' },
+            { title: 'Salary', field: 'salary', type: 'numeric' },
+            { title: 'Location', field: 'location' },
+            {
+                title: 'Job Title',
+                field: 'jobTitle',
+                lookup: { 34: 'Math Metition', 63: 'Astrophysicist', 1: 'Director', 2: 'Ceo', 3: 'Software Developer', 4: 'Manager', 5: 'Janitor' },
+            },
+        ],
+        data: [
+            
+        ],
+    });
